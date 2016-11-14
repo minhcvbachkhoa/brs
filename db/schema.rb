13156589040635b26e66c31fd4b2d13c7655d68e
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-11-14 10:29:01 +0700

    Request

--- db/schema.rb
+++ db/schema.rb
@@ -99,8 +99,10 @@ ActiveRecord::Schema.define(version: 20161107100702) do
     t.string   "book_title"
     t.date     "book_publish_date"
     t.string   "book_author"
+    t.integer  "user_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
