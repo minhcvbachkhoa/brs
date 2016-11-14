8c2656f5c4721db2aab348e20bb9c87636b1fac8
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-11-14 15:53:43 +0700

    validate

--- db/schema.rb
+++ db/schema.rb
@@ -119,16 +119,16 @@ ActiveRecord::Schema.define(version: 20161107100702) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false
     t.string   "image"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
